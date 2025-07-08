38baab01a0c54a314d781ced3b1497f9487aa34f
Dhananjay Maurya <dhananjaym182@gmail.com>
2025-07-08 21:45:43 +0530

    Create 700.sql

---
+++ mysql/700.sql
@@ -0,0 +1,8 @@
+-- Standard SQL queries
+UPDATE settings SET value = '{\"version\":\"7.0.0\", \"code\":\"700\"}' WHERE `key` = 'product_info';
+-- SEPARATOR --
+
+-- EXTENDED SEPARATOR --
+
+-- Extended license SQL queries
+-- SEPARATOR --
